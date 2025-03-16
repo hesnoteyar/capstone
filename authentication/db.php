4f9fd45b0d428f3f09ff047765e8fd7f96f8ae71
Jayar <jrsaranza92@gmail.com>
2025-03-16 23:39:41 +0800

    test db

--- authentication/db.php
+++ authentication/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost'; // Use 'localhost' for Hostinger, unless given a different hostname
 $db = 'u697061521_abaracing'; // Your database name
-$user = 'u697061521_abaracing'; // Your database username
-$pass = 'Aba_R@c1ng'; 
+$user = 'u697061521_root'; // Your database username
+$pass = 'jM!D#~i^H3'; 
 
 $conn = new mysqli($host, $user, $pass, $db);
 
