5abe74f22f3e177cf3c244f8fc76054764b11826
hesnoteyar <jrsaranza92@gmail.com>
2025-03-17 15:51:54 +0800

    include '..\authentication\db.php';

--- page/shop.php
+++ page/shop.php
@@ -2,7 +2,7 @@
 ob_start();
 session_start();
 include 'topnavbar.php';
-include '\authentication\db.php';
+include '..\authentication\db.php';
 
 $category = isset($_GET['category']) ? $_GET['category'] : 'All';
 $priceRange = isset($_GET['price_range']) ? $_GET['price_range'] : 10000;
