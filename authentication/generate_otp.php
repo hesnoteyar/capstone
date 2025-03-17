e180f119fcc9977140fa1297fce712633fa2293d
Jayar <jrsaranza92@gmail.com>
2025-03-17 19:57:09 +0800

    doneee

--- authentication/generate_otp.php
+++ authentication/generate_otp.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include 'db.php'; // Include your database connection file
-require '/vendor/autoload.php';
+require '../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -42,7 +42,7 @@ if (isset($_SESSION['email'])) {
             $mail->send();
 
             $_SESSION['success_message'] = "OTP sent to your email.";
-            header("Location: ..\authentication\otp_input.php");
+            header("Location: ../authentication/otp_input.php");
         } catch (Exception $e) {
             $_SESSION['error_message'] = "Error sending OTP: " . $mail->ErrorInfo;
             header("Location: ../index.php");
