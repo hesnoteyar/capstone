b3155ff217a1e2eea003c5403f6525e50f064a9f
Jayar <jrsaranza92@gmail.com>
2025-03-17 22:21:32 +0800

    wassup

--- employee/employee_payroll.php
+++ employee/employee_payroll.php
@@ -29,10 +29,9 @@ $overtime_rate = 97;
 // Fetch total hours worked and overtime hours in the current month from the attendance table
 $current_month = date('Y-m');
 $total_hours_query = "SELECT SUM(total_hours) AS total_hours, SUM(overtime_hours) AS overtime_hours 
-                      FROM attendance 
-                      WHERE employee_id = ? 
-                      AND DATE_FORMAT(date, '%Y-%m') COLLATE utf8mb4_general_ci = ?";
-$total_hours_stmt = $conn->prepare($total_hours_query);
+                     FROM attendance 
+                     WHERE employee_id COLLATE utf8mb4_unicode_ci = ? 
+                     AND DATE_FORMAT(date, '%Y-%m') = ?";$total_hours_stmt = $conn->prepare($total_hours_query);
 $total_hours_stmt->bind_param("is", $employee_id, $current_month);
 $total_hours_stmt->execute();
 $total_hours_stmt->bind_result($total_hours, $overtime_hours);
