422bf441a625f2ea2528c0bf775600ed821f858d
Jayar <jrsaranza92@gmail.com>
2025-03-18 21:54:14 +0800

    asdasd

--- page/model.php
+++ page/model.php
@@ -4,7 +4,6 @@ include '../authentication/db.php';
 include '../page/topnavbar.php';
 
 // Assuming user_id is stored in the session
-
 ?>
 
 <!DOCTYPE html>
@@ -17,12 +16,26 @@ include '../page/topnavbar.php';
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
     <title>Purchase History</title>
     <style>
-
+        /* Ensures the footer stays at the bottom */
+        html, body {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+        }
+        .content {
+            flex: 1;
+        }
     </style>
-
 </head>
 <body class="bg-base-100 text-base-content">
 
+
+    <div class="content">
+        <!-- Your page content goes here -->
+    </div>
+
     <?php include '../page/footer.php'; ?>
+
 </body>
 </html>
